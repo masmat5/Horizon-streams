fd8b4731572ec26e85a51abc39b75ee7e929648a
Muhammad Asmat <muhammadasmat508@gmail.com>
2026-05-20 20:39:50 +0500

    update speed

--- pages/_app.js
+++ pages/_app.js
@@ -2,5 +2,10 @@ import '../styles/globals.css';
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+    </>
+  )
 }
